ee3170904916d265565b3e26811e99620a55980d
stoyangalchev <galchev98@gmail.com>
2024-06-24 08:57:01 +0300

    Update timer

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,7 +12,7 @@ const Main = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 4000);
+    }, 1000);
 
     return () => clearTimeout(timer);
   }, []);
