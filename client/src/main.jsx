23dd0a819c4897921ad58080b20bc98f1a8eaf08
stoyangalchev <galchev98@gmail.com>
2024-06-24 22:24:03 +0300

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
-import "./index.css";
+import "./main.css";
 import React, { useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import reportWebVitals from "./reportWebVitals.js";
 import { BrowserRouter } from "react-router-dom";
-import {WheelLoadingSpinner} from "./utils/WheelLoadingSpinner/WheelLoadingSpinner.jsx";
+import { WheelLoadingSpinner } from "./utils/WheelLoadingSpinner/WheelLoadingSpinner.jsx";
 
 const Main = () => {
   const [isLoading, setIsLoading] = useState(true);
