17da1f8c43e03af1675c3e33e8b8cbc60cb61570
stoyangalchev <galchev98@gmail.com>
2024-06-24 08:54:26 +0300

    Create functionality to fetch data to subscribe email

---
+++ client/src/services/subscriptionService.js
@@ -0,0 +1,7 @@
+import * as fetchData from "./fetchData";
+
+const baseUrl = "http://localhost:3005/subscribe";
+
+export const saveSubscription = async (data) => {
+  return await fetchData.post(baseUrl, data);
+};
