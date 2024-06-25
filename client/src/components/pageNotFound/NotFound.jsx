3c6b2477461e50e66e3e4580983c1c73409256bd
stoyangalchev <galchev98@gmail.com>
2024-06-25 15:38:05 +0300

    Create page when have wrong url

---
+++ client/src/components/pageNotFound/NotFound.jsx
@@ -0,0 +1,14 @@
+import { scrollToTop } from '../globalComponents/ScrollArrow';
+import styles from './NotFound.module.css'
+
+export const NotFound = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.contentWrapper}>
+                <h1 className={styles.heading}>The Page you're looking for <span className={styles.highlight}>does not exist!</span></h1>
+                <img className={styles.img} src="/img/404.jpg" alt="notFound" />
+                <p className={styles.paragraph}>Seems like you're lost. Use our navigation bar to find your way. <i className="far fa-smile-beam"></i></p>
+            </div>
+        </div>
+    );
+}
