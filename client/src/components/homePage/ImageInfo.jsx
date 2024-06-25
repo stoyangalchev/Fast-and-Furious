7a8a5411c3421fa1bb132765b4fa4f2ddf832af1
stoyangalchev <galchev98@gmail.com>
2024-06-25 15:38:27 +0300

    Create ImageInfo component on home page

---
+++ client/src/components/homePage/ImageInfo.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import styles from "./ImageInfo.module.css";
+
+export const ImageInfo = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.imageContainer}>
+                <img className={styles.img} src="img/ff-five.jpeg" alt="ff-five" />
+                <div className={styles.overlayInfo}>
+                    <div className={styles.textContainer}>
+                        <i className={`fab fa-react ${styles.icon}`}></i>
+                        <h1 className={styles.heading}>Chose your Reactive car</h1>
+                        <Link to="/cars" className={styles.btn}>
+                            Browse cars
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
