3660067886be0dac6c3112a8228773b44ca92a7a
stoyangalchev <galchev98@gmail.com>
2024-06-26 12:36:26 +0300

    Update express.js

--- server/config/express.js
+++ server/config/express.js
@@ -1,28 +1,23 @@
-/*const cors = require('cors');
-const whitelist = ['http://localhost:3000'];
-
-module.exports = (app, express) => {
-  app.use(express.static('public'));
-  app.use(cors({ origin: whitelist, credentials: true }));
-  app.use(express.json());
-
-  app.use((error, req, res, next) => {
-    if (res.headerSent) {
-      return next(error);
-    }
-    res.status(error.code || 500).json({ message: error.message || 'An unknown error occurred!' });
-  });
-};*/
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 module.exports = (app) => {
-    app.use(
-        cors({
-            credentials: true,
-            origin: 'http://localhost:3000',
-            allowedHeaders: ['Content-Type', 'X-Authorization']
-        })
-    );
-    app.use(express.json());
+  app.use(
+    cors({
+      credentials: true,
+      origin: function (origin, callback) {
+        const allowedOrigins = [
+          "http://localhost:3000",
+          "http://localhost:5173",
+        ];
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+          callback(null, true);
+        } else {
+          callback(new Error("CORS policy violation"));
+        }
+      },
+      allowedHeaders: ["Content-Type", "X-Authorization"],
+    })
+  );
+  app.use(express.json());
 };
