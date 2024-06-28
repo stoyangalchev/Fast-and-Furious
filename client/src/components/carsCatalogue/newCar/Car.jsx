30b8186fb095c80d337c1b980ce4e2f38a530bc9
stoyangalchev <galchev98@gmail.com>
2024-06-28 09:18:15 +0300

    Create Car.jsx

---
+++ client/src/components/carsCatalogue/newCar/Car.jsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+
+import { useContext } from "react";
+import { AuthContext } from "../../../contexts/AuthContext";
+
+import styles from "./Car.module.css";
+
+
+export const Car = ({ car }) => {
+  const { isAuthenticated } = useContext(AuthContext);
+  return (
+    <section className={styles.container}>
+      <div className={styles.wrapper}>
+        <img className={styles.img} src={car.imageUrl} alt="carPic" />
+        <div className={styles.detailsWrapper}>
+          <div className={styles.row1}>
+            <div className={styles.itemRow1}>
+              <span>
+                <span className={styles.highlight}>Category:</span>{" "}
+                {car.category}{" "}
+              </span>
+            </div>
+            <div className={styles.itemRow1}>
+              <span>
+                <i className={`far fa-calendar-alt ${styles.highlight}`}></i>{" "}
+                {car.date}
+              </span>
+            </div>
+          </div>
+          <h4 className={styles.heading}>{car.name}</h4>
+          {isAuthenticated && (
+            <Link className={styles.btn} to={`/cars/${car._id}`}>
+              See more{" "}
+              <i className="bi bi-arrow-right" ></i>
+            </Link>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
