82599911caab604cc40463f1840980e4fc3d0897
stoyangalchev <galchev98@gmail.com>
2024-07-09 20:37:23 +0300

    Create text which response send on home page

--- server/index.js
+++ server/index.js
@@ -29,22 +29,32 @@ const start = async () => {
 };
 start();
 */
-const express = require('express');
+const express = require("express");
 
-const expressConfig = require('./config/express');
-const databaseConfig = require('./config/db');
-const routesConfig = require('./config/routes');
-const middlewaresConfig = require('./config/middlewares.js');
+const expressConfig = require("./config/express");
+const databaseConfig = require("./config/db");
+const routesConfig = require("./config/routes");
+const middlewaresConfig = require("./config/middlewares.js");
 
 start();
 
 async function start() {
-    const app = express();
+  const app = express();
 
-    expressConfig(app);
-    await databaseConfig(app);
-    await middlewaresConfig(app);
-    routesConfig(app);
+  expressConfig(app);
+  await databaseConfig(app);
+  await middlewaresConfig(app);
+  routesConfig(app);
 
-    app.listen(3005, () => console.log(`Application started at http://localhost:3005...`));
+  app.get("/", (req, res) => {
+    res.send(`
+        <h1 style=" text-align: center; font-family: Arial, sans-serif; margin:200px; font-size:80px">
+            Server is READY to use!
+        </h1>
+    `);
+  });
+
+  app.listen(3005, () =>
+    console.log(`Application started at http://localhost:3005...`)
+  );
 }
