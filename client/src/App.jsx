33f2a016ac37e3683da6eb0d3e05929623340956
stoyangalchev <galchev98@gmail.com>
2024-07-19 08:53:53 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,8 +9,10 @@ import { AuthenticatedRouting } from "./components/authRoutes/AuthenticatedRouti
 import { Team } from "./components/teamInfo/Team";
 import { AddNewCar } from "./components/carsCatalogue/newCar/AddNewCar";
 import { Catalogue } from "./components/carsCatalogue/Catalogue";
-// import { EditCar } from "./components/carsCatalogue/editCar/Editcar";
-// import { CarDetails } from "./components/carsCatalogue/carsDetails/CarDetails";
+import { EditCar } from "./components/carsCatalogue/editCar/Editcar";
+import { CarDetails } from "./components/carsCatalogue/carsDetails/CarDetails";
+import { AddComment } from "./components/carsCatalogue/comments/AddComment";
+import { Likedcars } from "./components/carsCatalogue/mycars/Likedcars";
 import { AboutUs } from "./components/homePage/AboutUs";
 import { Footer } from "./components/globalComponents/Footer";
 import { ImageInfo } from "./components/homePage/ImageInfo";
@@ -19,12 +21,9 @@ import { DownBar } from "./components/homePage/DownBar";
 import { Login } from "./components/user/Login";
 import { Register } from "./components/user/Register";
 import { NotFound } from "./components/pageNotFound/NotFound";
-// import { AddComment } from "./components/carsCatalogue/comments/AddComment";
 import { Logout } from "./components/user/Logout";
- import { Mycars } from "./components/carsCatalogue/mycars/Mycars";
-// import { Likedcars } from "./components/carsCatalogue/mycars/Likedcars";
+import { Mycars } from "./components/carsCatalogue/mycars/Mycars";
 import { ScrollArrow } from "./components/globalComponents/ScrollArrow";
-
 
 import ErrorBoundary from "./components/errorBoundary/ErrorBoudary";
 
@@ -59,7 +58,10 @@ function App() {
             <Route element={<AuthenticatedRouting />}>
               <Route path="/new-car" element={<AddNewCar />} />
               <Route path="/my-cars" element={<Mycars />} />
-
+              <Route path="/liked-cars" element={<Likedcars />} />
+              <Route path="/comments/:carId" element={<AddComment />} />
+              <Route path="/cars/:carId" element={<CarDetails />} />
+              <Route path="/cars/:carId/edit" element={<EditCar />} />
             </Route>
 
           </Routes>
@@ -72,48 +74,6 @@ function App() {
       </AuthProvider>
 
     </ErrorBoundary>
-
-
-
-
-
-
-    //       <Route
-    //         path="/"
-    //         element={
-    //           <>
-    //             <ImageInfo />
-    //             <AboutUs />
-    //             <Catalogue />
-    //             <Team />
-    //           </>
-    //         }
-    //       />
-
-    //       <Route path="/cars/" element={<Catalogue />} />
-    //       <Route path="/team" element={<Team />} />
-    //       <Route path="/login" element={<Login />} />
-    //       <Route path="/register" element={<Register />} />
-    //       <Route path="/logout" element={<Logout />} />
-
-    //       <Route element={<AuthenticatedRouting />}>
-    //         <Route path="/new-car" element={<AddNewCar />} />
-    //         <Route path="/my-cars" element={<Mycars />} />
-    //         <Route path="/liked-cars" element={<Likedcars />} />
-    //         <Route path="/comments/:carId" element={<AddComment />} />
-    //         <Route path="/cars/:carId" element={<CarDetails />} />
-    //         <Route path="/cars/:carId/edit" element={<EditCar />} />
-    //       </Route>
-
-
-
-
-
-
-
-
-
-
 
 
 
