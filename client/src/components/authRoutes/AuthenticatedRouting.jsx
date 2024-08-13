feb988fe790106a4cdb242e747d3c6509f9b7997
stoyangalchev <galchev98@gmail.com>
2024-08-13 15:00:47 +0300

    Add untracked files and directories

--- client/src/components/authRoutes/AuthenticatedRouting.jsx
+++ client/src/components/authRoutes/AuthenticatedRouting.jsx
@@ -5,7 +5,6 @@ import { Navigate, Outlet, useLocation } from "react-router-dom";
 export const AuthenticatedRouting = ({ children }) => {
   const { isAuthenticated } = useContext(AuthContext);
   const location = useLocation();
-
   if (!isAuthenticated) {
     return <Navigate to="/login" state={{ from: location }} />;
   }
