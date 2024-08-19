57bf1fa1e0fc24f0e96fb82254fd72cc38042038
stoyangalchev <galchev98@gmail.com>
2024-08-19 18:26:41 +0300

    Update fetchData.js

--- client/src/services/fetchData.js
+++ client/src/services/fetchData.js
@@ -1,28 +1,33 @@
-import { getAuthToken } from './getToken'
+import { getAuthToken } from "./getToken";
 
 export const fetchData = async (method, url, data) => {
-    const response = await fetch(url, {
-        method,
-        headers: {
-            'content-type': 'application/json',
-            'X-Authorization': getAuthToken()
-        },
-        body: JSON.stringify(data)
-    });
+  const response = await fetch(url, {
+    method,
+    headers: {
+      "content-type": "application/json",
+      "X-Authorization": getAuthToken(),
+    },
+    body: JSON.stringify(data),
+  });
 
-    const result = await response.json().catch(() => response.text());
+  const contentType = response.headers.get("content-type");
+  let result;
 
-    if (response.ok) {
-        return result;
-    } else {
-        throw result;
-    }
+  if (contentType && contentType.includes("application/json")) {
+    result = await response.json();
+  } else {
+    result = await response.text();
+  }
+
+  if (response.ok) {
+    return result;
+  } else {
+    throw result;
+  }
 };
 
-
-
-export const get = fetchData.bind(null, 'GET');
-export const put = fetchData.bind(null, 'PUT');
-export const post = fetchData.bind(null, 'POST');
-export const remove = fetchData.bind(null, 'DELETE');
-export const patch = fetchData.bind(null, 'PATCH');
+export const get = fetchData.bind(null, "GET");
+export const put = fetchData.bind(null, "PUT");
+export const post = fetchData.bind(null, "POST");
+export const remove = fetchData.bind(null, "DELETE");
+export const patch = fetchData.bind(null, "PATCH");
