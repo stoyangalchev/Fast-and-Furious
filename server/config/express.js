8a79c9f75b1d9611456ce1a7c7e3a2bb8b34bd9c
stoyangalchev <galchev98@gmail.com>
2024-08-19 15:51:42 +0300

    Update for deployment

--- server/config/express.js
+++ server/config/express.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+// Load environment variables from .env file
+dotenv.config();
 
 module.exports = (app) => {
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    process.env.TOKEN_SECRET,
+    process.env.DB_CONNECTION_STRING,
+  ];
+
   app.use(
     cors({
       credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE"], 
       origin: function (origin, callback) {
-        const allowedOrigins = [
-          "http://localhost:3000",
-          "http://localhost:5173",
-        ];
         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
           callback(null, true);
         } else {
