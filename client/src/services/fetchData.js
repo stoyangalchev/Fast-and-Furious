9e767f432a29131195430848e379fc3a1f1700b2
stoyangalchev <galchev98@gmail.com>
2024-08-19 16:15:45 +0300

    Update fetchData.js

--- client/src/services/fetchData.js
+++ client/src/services/fetchData.js
@@ -1,28 +1,20 @@
-import { getAuthToken } from './getToken'
+import { getAuthToken } from "./getToken";
 
 export const fetchData = async (method, url, data) => {
-    const response = await fetch(url, {
-        method,
-        headers: {
-            'content-type': 'application/json',
-            'X-Authorization': getAuthToken()
-        },
-        body: JSON.stringify(data)
-    });
+  const response = await fetch(url, {
+    method,
+    headers: {
+      "content-type": "application/json",
+      "X-Authorization": getAuthToken(),
+    },
+    body: JSON.stringify(data),
+  });
 
-    const result = await response.json();
+  const result = await response.json();
 
-    if (response.ok) {
-        return result;
-    } else {
-        throw result;
-    }
+  if (response.ok) {
+    return result;
+  } else {
+    throw result;
+  }
 };
-
-
-
-export const get = fetchData.bind(null, 'GET');
-export const put = fetchData.bind(null, 'PUT');
-export const post = fetchData.bind(null, 'POST');
-export const remove = fetchData.bind(null, 'DELETE');
-export const patch = fetchData.bind(null, 'PATCH');
