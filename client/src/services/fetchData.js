d0990c872d86a0783e155f8bfb85406b5fc4518f
stoyangalchev <galchev98@gmail.com>
2024-08-19 16:16:26 +0300

    Update fetchData.js

--- client/src/services/fetchData.js
+++ client/src/services/fetchData.js
@@ -1,20 +1,32 @@
 import { getAuthToken } from "./getToken";
 
 export const fetchData = async (method, url, data) => {
-  const response = await fetch(url, {
-    method,
-    headers: {
-      "content-type": "application/json",
-      "X-Authorization": getAuthToken(),
-    },
-    body: JSON.stringify(data),
-  });
+  try {
+    const response = await fetch(url, {
+      method,
+      headers: {
+        "content-type": "application/json",
+        "X-Authorization": getAuthToken(),
+      },
+      body: JSON.stringify(data),
+    });
 
-  const result = await response.json();
+    const contentType = response.headers.get("content-type");
+    let result;
 
-  if (response.ok) {
-    return result;
-  } else {
-    throw result;
+    if (contentType && contentType.includes("application/json")) {
+      result = await response.json();
+    } else {
+      result = await response.text(); // Handle non-JSON responses
+    }
+
+    if (response.ok) {
+      return result;
+    } else {
+      throw new Error(result);
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
   }
 };
