ae5e90e3e7fdd1442be3be8b54f83cf61c1c9d8c
stoyangalchev <galchev98@gmail.com>
2024-08-20 20:23:41 +0300

    Update fetchData.js

--- client/src/services/fetchData.js
+++ client/src/services/fetchData.js
@@ -1,28 +1,61 @@
-import { getAuthToken } from './getToken'
+// import { getAuthToken } from './getToken'
+
+// export const fetchData = async (method, url, data) => {
+//     const response = await fetch(url, {
+//         method,
+//         headers: {
+//             'content-type': 'application/json',
+//             'X-Authorization': getAuthToken()
+//         },
+//         body: JSON.stringify(data)
+//     });
+
+//     const result = await response.json().catch(() => response.text());
+
+//     if (response.ok) {
+//         return result;
+//     } else {
+//         throw result;
+//     }
+// };
+
+
+
+// export const get = fetchData.bind(null, 'GET');
+// export const put = fetchData.bind(null, 'PUT');
+// export const post = fetchData.bind(null, 'POST');
+// export const remove = fetchData.bind(null, 'DELETE');
+// export const patch = fetchData.bind(null, 'PATCH');
+import { getAuthToken } from "./getToken";
 
 export const fetchData = async (method, url, data) => {
-    const response = await fetch(url, {
-        method,
-        headers: {
-            'content-type': 'application/json',
-            'X-Authorization': getAuthToken()
-        },
-        body: JSON.stringify(data)
-    });
+  const response = await fetch(url, {
+    method,
+    headers: {
+      "content-type": "application/json",
+      "X-Authorization": getAuthToken(),
+    },
+    body: method !== "GET" ? JSON.stringify(data) : undefined, 
+  });
 
-    const result = await response.json().catch(() => response.text());
+  const text = await response.text(); 
 
-    if (response.ok) {
-        return result;
-    } else {
-        throw result;
-    }
+  let result;
+  try {
+    result = JSON.parse(text); 
+  } catch (e) {
+    result = text; 
+  }
+
+  if (response.ok) {
+    return result;
+  } else {
+    throw result;
+  }
 };
 
-
-
-export const get = fetchData.bind(null, 'GET');
-export const put = fetchData.bind(null, 'PUT');
-export const post = fetchData.bind(null, 'POST');
-export const remove = fetchData.bind(null, 'DELETE');
-export const patch = fetchData.bind(null, 'PATCH');
+export const get = fetchData.bind(null, "GET");
+export const put = fetchData.bind(null, "PUT");
+export const post = fetchData.bind(null, "POST");
+export const remove = fetchData.bind(null, "DELETE");
+export const patch = fetchData.bind(null, "PATCH");
