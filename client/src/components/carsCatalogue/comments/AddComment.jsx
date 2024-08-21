2a8f5f4e4f6176574345121fbf11d58c8d5eb91d
stoyangalchev <galchev98@gmail.com>
2024-08-21 17:40:34 +0300

    Add name to define  comment component sent direction

--- client/src/components/carsCatalogue/comments/AddComment.jsx
+++ client/src/components/carsCatalogue/comments/AddComment.jsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react';
 import * as commentsService from '../../../services/commentsService'
 import { useNavigate, useParams } from 'react-router-dom';
 import { scrollToTop } from '../../globalComponents/ScrollArrow';
-
+import * as carsService from '../../../services/carsService';
 import styles from './AddComment.module.css'
 
 export const AddComment = () => {
     const redirect = useNavigate();
-    const {carId} = useParams();
+    const { carId } = useParams();
+    const [car, setCar] = useState("");
     const [comment, setComment] = useState("");
     const [author, setAuthor] = useState("");
     const [serverError, setServerError] = useState(null);
@@ -16,6 +17,14 @@ export const AddComment = () => {
         scrollToTop();
         const authorName = JSON.parse(localStorage.getItem('auth'));
         setAuthor(authorName.username);
+
+        carsService.getcarDetails(carId)
+            .then((car) => {
+                setCar(car.name)
+            })
+            .catch(err => {
+                setServerError(err.message);
+            })
     }, [])
 
     const onCommentSubmit = async (e) => {
@@ -25,12 +34,12 @@ export const AddComment = () => {
             comment,
             author
         })
-        .then(() => {
-            redirect(`/cars/${carId}`)
-        })
-        .catch(err => {
-            setServerError(err.message);
-        })
+            .then(() => {
+                redirect(`/cars/${carId}`)
+            })
+            .catch(err => {
+                setServerError(err.message);
+            })
     }
 
     const handleCommentChange = (e) => {
@@ -49,40 +58,40 @@ export const AddComment = () => {
     const onCancel = () => {
         redirect(`/cars/${carId}`)
     }
-    
+
     return (
         <div className={styles.container}>
-        <div className={styles.contentWrapper}>
-            <div className={styles.col1}>
-                    <h1 className={styles.heading}>Add new <span className={styles.highlight}>Comment</span> to the car</h1>
-                <p className={styles.paragraph}>We are happy to hear from you. <i className="far fa-smile-beam"></i></p>
-            </div>
-            <div className={styles.col2}>
-        <div className={styles.col2Wrapper}>
-                    <form onSubmit={onCommentSubmit}>
-                        <div className={styles.form}>
-                            <div className={styles.maxWidthWrapper}>
-                                <label className={styles.newcarLbl} htmlFor="author">AUTHOR:
+            <div className={styles.contentWrapper}>
+                <div className={styles.col1}>
+                    <h1 className={styles.heading}>Add new <span className={styles.highlight}>Comment</span> to the {car}</h1>
+                    <p className={styles.paragraph}>We are happy to hear from you. <i className="far fa-smile-beam"></i></p>
+                </div>
+                <div className={styles.col2}>
+                    <div className={styles.col2Wrapper}>
+                        <form onSubmit={onCommentSubmit}>
+                            <div className={styles.form}>
+                                <div className={styles.maxWidthWrapper}>
+                                    <label className={styles.newcarLbl} htmlFor="author">AUTHOR:
                                         <input disabled type="text" id="author" className={styles.inputAuthor} value={author} />
-                                </label>
-           
-                            </div>
-                            <div className={styles.maxWidthWrapper}>
-                            <label className={styles.newcarLbl} htmlFor="description">COMMENT:</label>
+                                    </label>
+
+                                </div>
+                                <div className={styles.maxWidthWrapper}>
+                                    <label className={styles.newcarLbl} htmlFor="description">COMMENT:</label>
                                     <textarea className={`${styles.input} ${styles.inputComment}`} id="description" rows="5" value={comment} onChange={handleCommentChange} onBlur={handleCommentBlur} ></textarea>
+                                </div>
+                                <div className={styles.maxWidthWrapper}>
+                                    <p className={styles.errors}>{serverError}</p>
+                                    <button className={styles.btn} type="submit">ADD COMMENT</button>
+                                    <p></p>
+                                    <button className={styles.btn} type="button" onClick={onCancel}>CANCEL</button>
+                                </div>
                             </div>
-                            <div className={styles.maxWidthWrapper}>
-                            <p className={styles.errors}>{serverError}</p>
-                            <button className={styles.btn} type="submit">ADD COMMENT</button>
-                            <p></p>
-                            <button className={styles.btn} type="button" onClick={onCancel}>CANCEL</button>
-                            </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
-                </div>
-                </div>
-                </div>
-        );
-    }
+            </div>
+        </div>
+    );
+}
 
